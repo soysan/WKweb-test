9d7b408133bdd6a0e78cc7ef1f1edc34b6b9905a
Masayuki <soy2.san@gmail.com>
2021-02-23 16:04:47 +0900

    first

--- WKweb/ViewController.swift
+++ WKweb/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    var webView = WKWebView()
+    
+    @IBOutlet weak var toolBar: UIToolbar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        indicator.isHidden = true
+        webView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - toolBar.frame.size.height)
+        
+        view.addSubview(webView)
+        webView.navigationDelegate = self
+        
+        let url = URL(string: "https://www.kurashiru.com/")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        indicator.layer.zPosition = 2
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        indicator.isHidden = true
+        indicator.stopAnimating()
+    }
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        indicator.isHidden = false
+        indicator.startAnimating()
     }
 
-
+    @IBAction func back(_ sender: Any) {
+        webView.goBack()
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        webView.goForward()
+    }
 }
 
